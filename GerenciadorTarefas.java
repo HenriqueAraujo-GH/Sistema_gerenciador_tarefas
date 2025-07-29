71fad2ff35b9bf1e026b6d2fae687faf433896be
Henrique Araújo Oliveira <ao.henriquearaujo@gmail.com>
2025-07-29 15:54:55 -0300

    Update GerenciadorTarefas.java
    alterei para poder mudar o status de progressão da tarefa

--- GerenciadorTarefas.java
+++ GerenciadorTarefas.java
@@ -30,7 +30,7 @@ public class GerenciadorTarefas {
 	
 	public void marcarComoConcluida(int indice) {
 		if (indice >= 0 && indice < tarefas.size()) {
-			tarefas.get(indice).setConcluida(true);
+			tarefas.get(indice).setStatus(toString());
 			System.out.println("Tarefa marcada como concluida");
 		} else {
 			System.out.println("Índice inválido");
@@ -45,12 +45,52 @@ public class GerenciadorTarefas {
 			System.out.println("Índice inválido");
 		}
 	}
+
+	public void alterarStatustarefa() {
+		listatarefas():
+		if (tarefas.isEmpty()) return;
+
+		System.out.println("Digite o número da tarefa para alternar status: ");
+		int indice = scanner.nextInt() -1;
+		scanner.nextLine();
+
+		if (indice >= 0  && indice < tarefas.size()) {
+			Tarefa tarefa = tarefas.get(indice);
+			System.out.println("\nStatus atual: " + tarefa.getStatus());
+			System.out.println("1. Não iniciada");
+			System.out.println("2. Em progresso");
+			System.out.println("3. Concluída");
+			System.out.println("Escolha o novo status: ");
+
+			int opcaoStatus = scanner.nextInt();
+			scanner.nextLine();
+
+			switch (opcaoStatus) {
+				case 1:
+					tarefa.setStatus("Não iniciada");
+					break;
+				case 2:
+					tarefa.setStatus("Em progresso");
+					break;
+				case 3:
+					tarefa.setStatus("Concluída");
+					break;
+				default:
+					System.out.println("Opção inválida. Insira uma das opções disponiveis para alternar o Status da tarefa");
+
+					return;
+			}
+			System.out.println("Status da tarefa alterado");
+		} else {
+			System.out.println("Erro: Índice inválido");
+		}
+	}
 	
 	public void exibirMenu() {
 		System.out.println("\n=== Gerenciador de Tarefas ===");
 		System.out.println("1. Adicionar Tarefa");
 		System.out.println("2. Lista de Tarefas");
-		System.out.println("3. Marcar terfa como Concluida");
+		System.out.println("3. Alternar Status da tarefa");
 		System.out.println("4. Remover Tarefa");
 		System.out.println("5. Sair");
 		System.out.println("Escolha uma opção: ");
@@ -73,10 +113,7 @@ public class GerenciadorTarefas {
 				listaTarefas();
 				break;
 			case 3:
-				listaTarefas();
-				System.out.println("Digite o número da tarefaa marcar como concluida: ");
-				int indiceConcluir = scanner.nextInt() - 1;
-				marcarComoConcluida(indiceConcluir);
+				alternarStatusTarefa();
 				break;
 			case 4:
 				listaTarefas();
