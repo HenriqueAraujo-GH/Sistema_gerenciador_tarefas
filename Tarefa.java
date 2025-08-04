55b4f6f6fec704f1304fcf969bba63db268f9a7f
Henrique Araújo Oliveira <ao.henriquearaujo@gmail.com>
2025-08-04 18:24:40 -0300

    Tarefa.java

--- Tarefa.java
+++ Tarefa.java
@@ -1,14 +1,24 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
 public class Tarefa {
 	private String descricao;
 	private String status;
+	private LocalDate data;
 	
-	public Tarefa(String descricao) {
+	public Tarefa(String descricao, LocalDate data) {
 		this.descricao = descricao;
 		this.status = "Não iniciada";
+		this.data = data;
 	}
 	
 	public String getDescricao() {
 		return descricao;
+	}
+	
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
 	}
 	
 	public String getStatus() {
@@ -19,20 +29,28 @@ public class Tarefa {
 		this.status = status;
 	}
 	
+	public LocalDate getData() {
+		return data;
+	}
+	
+	public void setData(LocalDate data) {
+		this.data =  data;
+	}
+	
 	@Override
 	public String toString() {
 		String retorno;
 		switch (status) {
-			case "Concluída":
-				retorno = "[C]";
-				break;
-			case "Em progresso":
-				retorno = "[P]";
-				break;
-			default:
-				retorno = "[ ]";
-	}
-	return retorno + " " + descricao + " (" + status + ")";
+		case "Concluída":
+			retorno = "[C]";
+			break;
+		case "Em progresso":
+			retorno = "[P]";
+			break;
+		default:
+			retorno = "[ ]";
+		}
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        return String.format("%s %s | Data: %s | Local: %s | Status: %s", descricao, data.format(formatter), status);
 	}
 }
-
